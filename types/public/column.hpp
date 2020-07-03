2ceff33d19b0863c3eae44f4d7a222a5603e30e0
Christian Redeker <c.redeker@live.de>
2020-07-03 13:01:42 +0200

    add classes Row and Column

---
+++ types/public/column.hpp
@@ -0,0 +1,8 @@
+#ifndef SUDOKU_SOLVER_COLUMN_H
+#define SUDOKU_SOLVER_COLUMN_H
+
+#include <cell_container.hpp>
+
+class Column : public CellContainer {};
+
+#endif//SUDOKU_SOLVER_COLUMN_H
---
+++ types/public/row.hpp
@@ -0,0 +1,8 @@
+#ifndef SUDOKU_SOLVER_ROW_H
+#define SUDOKU_SOLVER_ROW_H
+
+#include <cell_container.hpp>
+
+class Row : public CellContainer {};
+
+#endif//SUDOKU_SOLVER_ROW_H
